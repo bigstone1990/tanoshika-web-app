2cd0696d211cc56d522fa3a77e68938eab9e2929
bigstone1990 <masatakalman@gmail.com>
2025-07-14 13:58:23 +0900

    マルチログインシステム 構築その7

--- resources/js/pages/admin/auth/confirm-password.tsx
+++ resources/js/pages/admin/auth/confirm-password.tsx
@@ -17,7 +17,7 @@ export default function ConfirmPassword() {
     const submit: FormEventHandler = (e) => {
         e.preventDefault();
 
-        post(route('password.confirm'), {
+        post(route('admin.password.confirm'), {
             onFinish: () => reset('password'),
         });
     };
--- resources/js/pages/admin/settings/password.tsx
+++ resources/js/pages/admin/settings/password.tsx
@@ -14,7 +14,7 @@ import { Label } from '@/components/ui/label';
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Password settings',
-        href: '/settings/password',
+        href: '/admin/settings/password',
     },
 ];
 
@@ -31,7 +31,7 @@ export default function Password() {
     const updatePassword: FormEventHandler = (e) => {
         e.preventDefault();
 
-        put(route('password.update'), {
+        put(route('admin.password.update'), {
             preserveScroll: true,
             onSuccess: () => reset(),
             onError: (errors) => {
